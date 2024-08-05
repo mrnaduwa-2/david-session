7569ca2a6bcafbfe1251b22088b81a159955196a
mrnaduwa-2 <nadunsadishka39@gmail.com>
2024-08-05 20:44:24 +0530

    Update wasiqr.js

--- wasiqr.js
+++ wasiqr.js
@@ -60,19 +60,16 @@ router.get('/', async (req, res) => {
 	
 				   let WASI_MD_TEXT = `
 ┏━━━━━━━━━━━━━━
-┃QUEEN_ANITA_V2 SESSION IS 
+┃MR.NADUWA-V4🧑‍💻SESSION IS 
 ┃SUCCESSFULLY
 ┃CONNECTED ✅🔥
 ┗━━━━━━━━━━━━━━━
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❶ || Creator = 𖥘 DAVID CYRIL 𖥘
+❶ || Creator = 𖥘 mr.naduwa𖥘
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-❷ || WhattsApp Channel = https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L
+❷ || WhattsApp Channel = https://chat.whatsapp.com/Ja4hbN0b8N0AzvJrs99wiB
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-Please Follow My Support Channel
-Wanna talk to me?👉 https://t.me/deecee_x 👈
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-©*2024-2099 David Cyril*
+©*2024-2099 mr.naduwa*
 _____________________________________
 	
 _Don't Forget To Give Star To My Repo_`
